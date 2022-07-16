66f5ef9b32238a539997ac3c4c4116672126d34c
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-16 19:57:46 +0530

    Added LogicalProblems file

---
+++ LogicalProblems/LogicalProblems/StopWatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LogicalProblems
+{
+    public class StopWatch
+    {
+        public static void stopWatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
